3c737d6b7d56ba1e99f57019a0556287a78ef5eb
pwn4all <66910989+pwn4all@users.noreply.github.com>
2022-11-17 14:41:20 +0900

    Update str2dict.py

--- python/str2dict.py
+++ python/str2dict.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 import json
 
-string_type_dict = '{"hello":"world", "key": 1234, "name":"john"}'
+filename = './temp.txt'
+str_lines = list()
+str_to_dict = dict()
 
-string_to_dict = json.loads(string_type_dict)
-print(type(string_to_dict))
-# print(string_to_dict['hello'])
-for key in string_to_dict.keys():
-    print(string_to_dict[key])
+with open(filename, 'r') as fd:
+    str_lines = fd.readlines()
+# print(str_lines)
 
-'''
-------------------------------------------------
-<class 'dict'>
-world
-1234
-john
-------------------------------------------------
-'''
+for str_line in str_lines:
+    str_to_dict = json.loads(str_line)
+    print(f'format : {type(str_to_dict)}')
+    print(f'dicts : {str_to_dict}')
+    print(f'keys : {str_to_dict.keys()}')
+    print(f'values : {str_to_dict.values()}')
+    print("\n============================\n")
+
+
+    
+## temp.txt #############################################
+## {"English": "hello world", "Spanish": "Hola Mundo", "French": "Bonjour le monde"}
+## {"English": "hello python", "Spanish": "bonjour python", "French": "hola pitón"}
+## temp.txt #############################################
