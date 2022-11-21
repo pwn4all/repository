f86de0549a6517cb480c4e7c4b44361be93eba4e
pwn4all <66910989+pwn4all@users.noreply.github.com>
2022-11-21 11:55:30 +0900

    Update str2dict.py

--- python/str2dict.py
+++ python/str2dict.py
@@ -9,17 +9,37 @@ with open(filename, 'r') as fd:
     str_lines = fd.readlines()
 # print(str_lines)
 
+line_cnt = 3
 for str_line in str_lines:
     str_to_dict = json.loads(str_line)
+    print("============================" * line_cnt)
     print(f'format : {type(str_to_dict)}')
     print(f'dicts : {str_to_dict}')
     print(f'keys : {str_to_dict.keys()}')
     print(f'values : {str_to_dict.values()}')
-    print("\n============================\n")
-
+    print("============================" * line_cnt)
 
     
-## temp.txt #############################################
-## {"English": "hello world", "Spanish": "Hola Mundo", "French": "Bonjour le monde"}
-## {"English": "hello python", "Spanish": "bonjour python", "French": "hola pitón"}
-## temp.txt #############################################
+    
+"""
+====================================================================================
+format : <class 'dict'>
+dicts : {'English': 'hello world', 'Spanish': 'Hola Mundo', 'French': 'Bonjour le monde'}
+keys : dict_keys(['English', 'Spanish', 'French'])
+values : dict_values(['hello world', 'Hola Mundo', 'Bonjour le monde'])
+====================================================================================
+====================================================================================
+format : <class 'dict'>
+dicts : {'English': 'hello python', 'Spanish': 'bonjour python', 'French': 'hola pitón'}
+keys : dict_keys(['English', 'Spanish', 'French'])
+values : dict_values(['hello python', 'bonjour python', 'hola pitón'])
+====================================================================================
+
+
+"""
+
+""" 
+## temp.txt
+{"English": "hello world", "Spanish": "Hola Mundo", "French": "Bonjour le monde"}
+{"English": "hello python", "Spanish": "bonjour python", "French": "hola pitón"}
+"""
