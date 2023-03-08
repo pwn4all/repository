c88f1b6b9b3e085bb4724eb1fa028e0848461ab4
pwn4all <66910989+pwn4all@users.noreply.github.com>
2023-03-08 09:28:16 +0900

    Update extract_sample.py

--- pandas/extract_sample.py
+++ pandas/extract_sample.py
@@ -31,3 +31,38 @@ print("[+] apply reset_index() =================================")
 print(df.sample(frac=1, random_state=1000, ignore_index=True).reset_index())
 print("[+] apply reset_index(drop=True) =================================")
 print(df.sample(frac=1, random_state=1000, ignore_index=False).reset_index(drop=True))
+
+
+
+''' result
+[+] extract 3 rows ======================================
+   number        message
+4     500     hello perl
+3     400  hello flutter
+1     200   hello python
+[+] extract 50 percent ==================================
+   number     message
+4     500  hello perl
+2     300  hello java
+[+] apply random_state ==================================
+   number        message
+2     300     hello java
+1     200   hello python
+0     100    hello world
+4     500     hello perl
+3     400  hello flutter
+[+] apply reset_index() =================================
+   index  number        message
+0      0     300     hello java
+1      1     200   hello python
+2      2     100    hello world
+3      3     500     hello perl
+4      4     400  hello flutter
+[+] apply reset_index(drop=True) =================================
+   number        message
+0     300     hello java
+1     200   hello python
+2     100    hello world
+3     500     hello perl
+4     400  hello flutter
+'''
