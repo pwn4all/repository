9622e6bb514cf3ca04da02719f38b8a41c711580
pwn4all <66910989+pwn4all@users.noreply.github.com>
2023-04-12 14:53:52 +0900

    Update offline_pip.sh

--- python/offline_pip.sh
+++ python/offline_pip.sh
@@ -5,5 +5,8 @@ $ mkdir virtualenv
 $ pip download -d ./virtualenv virtualenv
 
 
+#################################################################################
 ## install package using local *.whl or *.tar.gz
-## ------------------------------------------------------------------------
+## link : https://pip.pypa.io/en/stable/cli/pip_install/
+#################################################################################
+$ python -m pip install --no-index --find-links=/home/user/virtualenv virtualenv
