be8ec2e36b5eb03beaf67d110f46459b6559db92
pwn4all <66910989+pwn4all@users.noreply.github.com>
2023-04-13 18:18:48 +0900

    Update install_mecab.sh

--- colab/install_mecab.sh
+++ colab/install_mecab.sh
@@ -12,7 +12,7 @@
 !curl -s https://raw.githubusercontent.com/teddylee777/machine-learning/master/99-Misc/01-Colab/mecab-colab.sh | bash
 or
 # below script
-!install-mecab-colab.sh
+!curl -s https://raw.githubusercontent.com/pwn4all/repository/master/colab/install-mecab-colab.sh | bash
 
 
 ##########################################################################################
@@ -33,49 +33,3 @@ print(okt.pos(sentence))
 print(mecab.nouns(sentence))
 print(mecab.morphs(sentence))
 print(mecab.pos(sentence))
-
-
-
-
-
-## =======================================================================================
-## install-mecab-colab.sh need to root permission
-## ---------------------------------------------------------------------------------------
-## https://bitbucket.org/eunjeon/mecab-ko-dic/src/master/
-## https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz
-## https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz
-## ---------------------------------------------------------------------------------------
-## https://bitbucket.org/eunjeon/mecab-python-0.996/src/master/
-## =======================================================================================
-#! /bin/bash
-cd /tmp && \
-wget "https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz" && \
-tar zxfv mecab-0.996-ko-0.9.2.tar.gz && \
-cd mecab-0.996-ko-0.9.2 && \
-./configure && \
-make && \
-make check && \
-make install && \
-ldconfig
-
-cd /tmp && \
-wget "https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz" && \
-apt install -y autoconf && \
-tar zxfv mecab-ko-dic-2.1.1-20180720.tar.gz && \
-cd mecab-ko-dic-2.1.1-20180720 && \
-./autogen.sh && \
-./configure && \
-make && \
-make install && \
-ldconfig
-
-cd /tmp && \
-git clone https://bitbucket.org/eunjeon/mecab-python-0.996.git && \
-cd mecab-python-0.996 && \
-python setup.py build && \
-python setup.py install
-
-
-## ---------------------------------------------------------------------------------------
-pip install konlpy
-## =======================================================================================
