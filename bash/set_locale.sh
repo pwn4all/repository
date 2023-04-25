a1cf597d1336c4268df75a22b95c9e6896b8212f
pwn4all <66910989+pwn4all@users.noreply.github.com>
2023-04-25 11:04:01 +0900

    Create set_locale.sh

---
+++ bash/set_locale.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+########################################################################################
+## view locale
+########################################################################################
+# locale
+LANG=
+LC_CTYPE="POSIX"
+LC_NUMERIC="POSIX"
+LC_TIME="POSIX"
+LC_COLLATE="POSIX"
+LC_MONETARY="POSIX"
+LC_MESSAGES="POSIX"
+LC_PAPER="POSIX"
+LC_NAME="POSIX"
+LC_ADDRESS="POSIX"
+LC_TELEPHONE="POSIX"
+LC_MEASUREMENT="POSIX"
+LC_IDENTIFICATION="POSIX"
+LC_ALL=
+
+
+########################################################################################
+## set locale
+########################################################################################
+# localedef -c -i ko_KR -f UTF-8 ko_KR.UTF-8
+# export LC_ALL=ko_KR.UTF-8
+
+# cat ~/.bashrc
+.
+.
+.
+## KOR
+export LC_ALL=ko_KR.UTF-8
