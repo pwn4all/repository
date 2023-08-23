2aea74be9e1b48c00a23e706321e84c315a334d2
pwn4all <66910989+pwn4all@users.noreply.github.com>
2023-08-23 10:32:25 +0900

    Update chg_encoding_iconv.sh

--- bash/chg_encoding_iconv.sh
+++ bash/chg_encoding_iconv.sh
@@ -1,6 +1,4 @@
-#!bin/bash
-
-
+```bash
 #################################################################
 ## find encoding
 ## windows : cp949
@@ -10,8 +8,9 @@ $ file --mime-encoding filename.txt
 
 $ file --mime-encoding filename.log
 ./filename.log: utf-8
+```
 
-
+```bash
 #################################################################
 ## change encoding
 #################################################################
@@ -22,3 +21,26 @@ $ iconv -c -f cp949 -t utf8 source.txt > source.log
 
 #Options controlling conversion problems:
 #  -c                          discard unconvertible characters
+```
+
+
+## 특정 디렉터리 내의 .txt 등 확장자 파일을 대상으로 인코딩 변환 스크립트
+```bash
+#!/bin/bash
+
+# 디렉토리 내의 모든 txt 파일을 순회하며 Encoding 변경
+if [ $# -ne 2 ]; then
+        echo "Usage: $0 From(ex.UTF-8) To(ex.EUC-KR)"
+        exit -1
+fi
+
+# 처리 대상 확장자 euckr/txt 등 수정 필요
+for txt_file in *.euckr; do
+    # 확장자를 제외한 파일명만 추출
+    filename="${txt_file%.*}"
+
+    echo $txt_file
+    # argv[1]인코딩을 argv[2] 인코딩으로 변경
+    /usr/bin/iconv -c -f $1 -t $2 $filename.euckr -o $filename.txt
+done
+```
