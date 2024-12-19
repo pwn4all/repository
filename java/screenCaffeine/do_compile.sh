977da2401a392bee1c71c4694c131f8caed90937
pwn4all <66910989+pwn4all@users.noreply.github.com>
2024-12-19 09:55:50 +0900

    Update do_compile.sh

--- java/screenCaffeine/do_compile.sh
+++ java/screenCaffeine/do_compile.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# JAVA_BIN Path
-JAVA_BIN="/opt/homebrew/opt/openjdk@21/bin"
-
 # Print usage
 usage() {
     echo "Usage: $0 <JavaFileNameWithoutExtension>"
@@ -34,18 +31,22 @@ fi
 # Get filename
 FILE_NAME=$1
 
-# copy config file to utils dir
-echo "Copying ${FILE_NAME}.conf..."
-check_file_exists ${FILE_NAME}.conf
-/bin/cp ${FILE_NAME}.conf $HOME/utils
-check_error "copy ${FILE_NAME}.conf failed."
+# create utils directory if it does not exist
+echo "Creating $HOME/utils directory if it does not exist..."
+mkdir -p "$HOME/utils/test"
+check_error "Failed to create $HOME/utils directory."
 
 # Check if a Java file exists
 check_file_exists "${FILE_NAME}.java"
 
-# Compiling
+# Compiling ScreenCaffeine
 echo "Compiling ${FILE_NAME}.java..."
-${JAVA_BIN}/javac "${FILE_NAME}.java"
+javac "${FILE_NAME}.java"
+check_error "Compilation failed."
+
+# Compiling PLIST_NAME
+echo "Compiling ${FILE_NAME}Plist.java..."
+javac "${FILE_NAME}Plist.java"
 check_error "Compilation failed."
 
 # Check if MANIFEST.MF file exists
@@ -53,17 +54,12 @@ check_file_exists "MANIFEST.MF"
 
 # create jar file
 echo "Creating ${FILE_NAME}.jar..."
-${JAVA_BIN}/jar cfm "${FILE_NAME}.jar" MANIFEST.MF "${FILE_NAME}.class"
+jar cfm "${FILE_NAME}.jar" MANIFEST.MF "${FILE_NAME}.class"
 check_error "JAR creation failed."
 
-# copy jar file to utils dir
-echo "Copying ${FILE_NAME}.jar..."
-# check_file_exists ${FILE_NAME}.jar
-/bin/cp ${FILE_NAME}.jar $HOME/utils
-check_error "copy ${FILE_NAME}.jar failed."
 
 # execute jar file
-echo "Running ${FILE_NAME}.jar..."
-${JAVA_BIN}/java -jar "${FILE_NAME}.jar"
-check_error "Execution failed."
+#echo "Running ${FILE_NAME}.jar..."
+# java -jar "${FILE_NAME}.jar"
+#check_error "Execution failed."
 
